6b21fc7b1457ccabfa5f7cc321c64ffa8ba6366d
Levene <gang.wang.china@gmail.com>
2024-09-04 18:30:49 +0800

    feat: xxx

--- image_hosting/def_pro_v1.go
+++ image_hosting/def_pro_v1.go
@@ -3,6 +3,6 @@ package imagehosting
 const (
 	ACCESS_KEY_ID     = "wCSYOmYFgJmttmWs"
 	ACCESS_KEY_SECRET = "QcXJXEXWr59zfvszfFd6AxY5KMnqrb"
-	BUCKET            = "ellabook-ai-pic"
-	Endpoint          = "oss-cn-hangzhou.aliyuncs.com"
+	BUCKET            = "ov-ai-pic"
+	Endpoint          = "oss-us-west-1.aliyuncs.com"
 )
